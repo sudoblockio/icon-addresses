77b4cade5d715719f3e61e52b77b31dcd24faebf
Pranav <pranav@geometrylabs.io>
2021-09-28 20:04:15 -0700

    remove gorm logging, enable kafka logging

--- src/crud/postgres.go
+++ src/crud/postgres.go
@@ -39,7 +39,18 @@ func getPostgresConn() *gorm.DB {
 }
 
 func createSession(dsn string) (*gorm.DB, error) {
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second,   // Slow SQL threshold
+			LogLevel:                  logger.Silent, // Log level
+			IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
+			Colorful:                  false,         // Disable color
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: newLogger})
 	if err != nil {
 		zap.S().Info("err:", err)
 	}
