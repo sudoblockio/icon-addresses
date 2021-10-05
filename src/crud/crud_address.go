abf85ac829a30c81fa4efc4b68a14292343a6439
Pranav <pranav@geometrylabs.io>
2021-10-05 15:16:19 -0700

    refactor upsert code

--- src/crud/crud_address.go
+++ src/crud/crud_address.go
@@ -134,96 +134,11 @@ func (m *AddressModel) UpsertOne(
 ) error {
 	db := m.db
 
-	// Create map[]interface{} with only non-nil fields
-	updateOnConflictValues := map[string]interface{}{}
-
-	// Loop through struct using reflect package
-	modelValueOf := reflect.ValueOf(*address)
-	modelTypeOf := reflect.TypeOf(*address)
-	for i := 0; i < modelValueOf.NumField(); i++ {
-		modelField := modelValueOf.Field(i)
-		modelType := modelTypeOf.Field(i)
-
-		modelTypeJSONTag := modelType.Tag.Get("json")
-		if modelTypeJSONTag != "" {
-			// exported field
-
-			// Check if field if filled
-			modelFieldKind := modelField.Kind()
-			isFieldFilled := true
-			switch modelFieldKind {
-			case reflect.String:
-				v := modelField.Interface().(string)
-				if v == "" {
-					isFieldFilled = false
-				}
-			case reflect.Int:
-				v := modelField.Interface().(int)
-				if v == 0 {
-					isFieldFilled = false
-				}
-			case reflect.Int8:
-				v := modelField.Interface().(int8)
-				if v == 0 {
-					isFieldFilled = false
-				}
-			case reflect.Int16:
-				v := modelField.Interface().(int16)
-				if v == 0 {
-					isFieldFilled = false
-				}
-			case reflect.Int32:
-				v := modelField.Interface().(int32)
-				if v == 0 {
-					isFieldFilled = false
-				}
-			case reflect.Int64:
-				v := modelField.Interface().(int64)
-				if v == 0 {
-					isFieldFilled = false
-				}
-			case reflect.Uint:
-				v := modelField.Interface().(uint)
-				if v == 0 {
-					isFieldFilled = false
-				}
-			case reflect.Uint8:
-				v := modelField.Interface().(uint8)
-				if v == 0 {
-					isFieldFilled = false
-				}
-			case reflect.Uint16:
-				v := modelField.Interface().(uint16)
-				if v == 0 {
-					isFieldFilled = false
-				}
-			case reflect.Uint32:
-				v := modelField.Interface().(uint32)
-				if v == 0 {
-					isFieldFilled = false
-				}
-			case reflect.Uint64:
-				v := modelField.Interface().(uint64)
-				if v == 0 {
-					isFieldFilled = false
-				}
-			case reflect.Float32:
-				v := modelField.Interface().(float32)
-				if v == 0 {
-					isFieldFilled = false
-				}
-			case reflect.Float64:
-				v := modelField.Interface().(float64)
-				if v == 0 {
-					isFieldFilled = false
-				}
-			}
-
-			if isFieldFilled == true {
-				updateOnConflictValues[modelTypeJSONTag] = modelField.Interface()
-			}
-		}
-	}
+	// map[string]interface{}
+	updateOnConflictValues := extractFilledFieldsFromModel(
+		reflect.ValueOf(*address),
+		reflect.TypeOf(*address),
+	)
 
 	// Upsert
 	db = db.Clauses(clause.OnConflict{
@@ -283,7 +198,7 @@ func StartAddressLoader() {
 			// Load to postgres //
 			//////////////////////
 			err = GetAddressModel().UpsertOne(newAddress)
-			zap.S().Debug("Loader=Address, Address=", newAddress.PublicKey, " - Upsert")
+			zap.S().Debug("Loader=Address, Address=", newAddress.PublicKey, " - Upserted")
 			if err != nil {
 				// Postgres error
 				zap.S().Info("Loader=Address, Address=", newAddress.PublicKey, " - FATAL")
