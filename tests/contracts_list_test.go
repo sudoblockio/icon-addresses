43f675f26fc9f63e82fe54ea19083304f85ea956
Pranav <pranav@geometrylabs.io>
2021-11-12 14:17:17 -0800

    add contracts list integration test

---
+++ tests/contracts_list_test.go
@@ -0,0 +1,42 @@
+package tests
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// List test
+func TestContractsEndpointList(t *testing.T) {
+	assert := assert.New(t)
+
+	addressesServiceURL := os.Getenv("ADDRESSES_SERVICE_URL")
+	if addressesServiceURL == "" {
+		addressesServiceURL = "http://localhost:8000"
+	}
+	addressesServiceRestPrefx := os.Getenv("ADDRESSES_SERVICE_REST_PREFIX")
+	if addressesServiceRestPrefx == "" {
+		addressesServiceRestPrefx = "/api/v1"
+	}
+
+	resp, err := http.Get(addressesServiceURL + addressesServiceRestPrefx + "/addresses/contracts")
+	assert.Equal(nil, err)
+	assert.Equal(200, resp.StatusCode)
+
+	defer resp.Body.Close()
+
+	// Test headers
+	assert.NotEqual("0", resp.Header.Get("X-TOTAL-COUNT"))
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	assert.Equal(nil, err)
+
+	bodyMap := make([]interface{}, 0)
+	err = json.Unmarshal(bytes, &bodyMap)
+	assert.Equal(nil, err)
+	assert.NotEqual(0, len(bodyMap))
+}
